468a0cbff482516b83a8ed2fc6363c06d61ba8ff
tambongslade <tambongslade17@gmail.com>
2023-02-03 14:42:11 -0800

    mui card completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
+              ),
+              Text(
+                'Tambong kersten',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter developer',
+                style: TextStyle(
+                  fontFamily: 'Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    '+ 237 670 52 74 26',
+                    style: TextStyle(
+                      fontFamily: 'Pro',
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    title: Text(
+                      'Tambongslade17@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Pro',
+                        fontSize: 40,
+                      ),
+                    ),
+                  )),
+            ],
+          )),
+    );
+  }
 }
